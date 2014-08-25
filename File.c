89e654e7cd8bc6b55ad6568c8655f6db55b69d26
kdeligian <kdeligian@Kostass-MacBook-Air.local>
2014-08-25 22:16:37 +0300

    Description

---
+++ File.c
@@ -0,0 +1,15 @@
+//
+//  File.c
+//  SharedTest
+//
+//  Created by Kostas Deligiannidis on 12/22/12.
+//  Copyright (c) 2012 Kostas Deligiannidis. All rights reserved.
+//
+
+#include <stdio.h>
+
+void mymod( int x )
+{
+    printf( "mymod: %d\n", 2*x );
+    
+}
